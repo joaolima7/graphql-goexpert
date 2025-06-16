09aaa29f78c1b22408434ec87c680ce0fa155c16
joaolima7 <jcgam3r69@gmail.com>
2025-06-16 19:16:21 -0300

    feat: add Category and Course models with corresponding mutations and queries
    - Introduced new models: Category and Course in models_gen.go.
    - Updated the Mutation type to include NewCategory and NewCourse types.
    - Implemented CreateCategory mutation in schema.resolvers.go to handle category creation.
    - Added Categories and Courses query resolvers with placeholders for future implementation.
    - Created a new database package for Category with methods to interact with the database.

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
@@ -11,18 +12,28 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/joaolima7/graphql-goexpert/graph"
+	"github.com/joaolima7/graphql-goexpert/internal/database"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: database.NewCategory(db),
+	}}))
 
 	srv.AddTransport(transport.Options{})
 	srv.AddTransport(transport.GET{})
